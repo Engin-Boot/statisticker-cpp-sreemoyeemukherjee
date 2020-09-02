eba3396bc9dcbff0e0f6a761215d26722beda60f
Sreemoyee Mukherjee <titlisreemoyee@gmail.com>
2020-09-02 14:57:00 +0530

    Including 'math.h' header file

--- stats-test.cpp
+++ stats-test.cpp
@@ -3,6 +3,7 @@
 #include "catch.hpp"
 #include "stats.h"
 #include <cmath>
+#include <math.h>
 TEST_CASE("reports average, minimum and maximum") {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
