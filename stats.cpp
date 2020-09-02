4a103c2d75e6331cf9863fb1e6bf653db199e51b
Sreemoyee Mukherjee <titlisreemoyee@gmail.com>
2020-09-02 14:49:39 +0530

    Reduced CCN to 3 for all files

--- stats.cpp
+++ stats.cpp
@@ -1,4 +1,6 @@
 #include "stats.h"
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
@@ -8,19 +10,15 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
     {
         s.average = NAN;
         s.min = NAN;
-        s.max = NAN;
+        s.max = NAN; 
         return s;
     }
     float sum=0;
-    s.min = vect[0];
-    s.max = vect[0];
+    s.min = *min_element(vect.begin(),vect.end());
+    s.max = *max_element(vect.begin(), vect.end());
     for (int i = 0; i < vect.size(); i++)
     {
         sum = sum + vect[i];
-        if (vect[i] < s.min)
-            s.min = vect[i];
-        if (vect[i] > s.max)
-            s.max = vect[i];
     }
     s.average = sum/vect.size();
     return s;
