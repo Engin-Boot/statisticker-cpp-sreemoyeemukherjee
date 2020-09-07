20983d02a824f523e780153228cdeb97c0d81c1c
Sreemoyee Mukherjee <titlisreemoyee@gmail.com>
2020-09-07 18:57:38 +0530

    Including NAN and error test cases

--- stats-test.cpp
+++ stats-test.cpp
@@ -5,15 +5,15 @@
 #include <cmath>
 #include <math.h>
 
-TEST_CASE("reports average, minimum and maximum") {
+TEST_CASE("when all inputs are real numbers of type float then report average, minimum and maximum to an accuracy of 0.001") {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    float epsilon = 0.001;
+    float epsilon = 0.001f;
     REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
-TEST_CASE("average is NaN for empty array") {
+TEST_CASE("average, min, max is NaN for empty array") {
     auto computedStats = Statistics::ComputeStatistics({});
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
@@ -23,4 +23,11 @@ TEST_CASE("average is NaN for empty array") {
     REQUIRE(isnan(computedStats.max)==true);
     REQUIRE(isnan(computedStats.min)==true);
     //Use http://www.cplusplus.com/reference/cmath/isnan/
+}
+TEST_CASE("when some elements in the input are NaN then calculate stats of remaining numbers") {
+    auto computedStats = Statistics::ComputeStatistics({NAN,1.5,NAN,3.0,4.5,NAN});
+    float epsilon = 0.001;
+    REQUIRE(std::abs(computedStats.average - 3.000) < epsilon);
+    REQUIRE(std::abs(computedStats.max - 4.5) < epsilon);
+    REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
--- stats.cpp
+++ stats.cpp
@@ -2,25 +2,32 @@
 #include <iostream>
 #include <algorithm>
 #include <math.h>
+#include <vector>
 using namespace std;
 
+vector<float> RemoveNAN(vector<float> vect)
+{
+   vect.erase(remove_if(vect.begin(),vect.end(),[](const auto& value) { return std::isnan(value); }),vect.end());
+    return vect;
+}
 Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
     //Implement statistics here
     Stats s;
-    if (vect.empty())   // Error condition
+    s.average = NAN;
+    s.min = NAN;
+    s.max = NAN;
+    vector<float> newvect = RemoveNAN(vect);
+    if (newvect.empty())  // Error condition
     {
-        s.average = NAN;
-        s.min = NAN;
-        s.max = NAN; 
         return s;
     }
     float sum=0;
-    s.min = *min_element(vect.begin(),vect.end());
-    s.max = *max_element(vect.begin(), vect.end());
-    for (int i = 0; (unsigned) i < vect.size(); i++)
+    s.min = *min_element(newvect.begin(),newvect.end());
+    s.max = *max_element(newvect.begin(), newvect.end());
+    for (int i = 0; (unsigned) i < newvect.size(); i++)
     {
-        sum = sum + vect[i];
+            sum = sum + newvect[i];
     }
-    s.average = sum/vect.size();
+    s.average = sum/newvect.size();
     return s;
 }
